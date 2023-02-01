92425dfbac272e41736cf3c8f116fe5b3ab2a74a
cklee90 <cklee90@gmail.com>
2023-02-01 14:25:02 +0900

    settings.py logging 설정 추가

---
+++ pybo/forms.py
@@ -0,0 +1,34 @@
+'''
+파일명 : forms.py
+설명 : html form 관리
+생성일 : 2023/02/01
+생성자 : ckair
+'''
+
+
+from django import  forms
+from pybo.models import Question, Answer
+
+class AnswerForm(forms.ModelForm):
+    class Meta:
+        model = Answer # 사용할 Answer model
+
+        fields = ['content']  # AnswerForm 에서 사용할 Answer model의 속성
+
+        labels = {
+            'content':'답변 내용'
+        }
+
+
+class QuestionForm(forms.ModelForm):
+    class Meta:
+        model = Question # 사용할 question model
+
+        fields = ['subject', 'content']  #QuestionForm 에서 사용할 question model 의 속성
+
+        labels = {
+            'subject':'제목',
+            'content':'내용',
+        }
+
+
--- pybo/views.py
+++ pybo/views.py
@@ -1,7 +1,29 @@
 from django.shortcuts import render,get_object_or_404, redirect
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseNotAllowed
 from django.utils import timezone
 from .models import  Question
+from .forms import QuestionForm, AnswerForm
+import logging
+
+def question_create(request):
+    '''질문 등록'''
+    logging.info('1. request.method:{}'.format(request.method))
+    if request.method == 'POST':
+        logging.info('2. question_create post')
+        # 저장
+        form = QuestionForm(request.POST)   # request.post 로 표시를 해줘야함.(subject, content 자동 생성)
+        logging.info('3. question_create post')
+        logging.info('4. form is valid():{}'.format(form.is_valid()))
+        if form.is_valid():  # form(질문등록) 이 유효하면
+            logging.info('4. form is valid():{}'.format(form.is_valid()))
+            question = form.save(commit=False)  # subject, content 만 저장(확정commit은 하지 않음)
+            question.create_date = timezone.now()
+            question.save()  #날짜 까지 생성해서 저장 ( commit)
+            return redirect('pybo:index')
+    else:
+        form = QuestionForm()
+    context = {'form': form}
+    return render(request,'pybo/question_form.html', context)
 
 def boot_menu(request):
     '''개발에 사용되는 임시 메뉴'''
@@ -18,25 +40,41 @@ def boot_list(request):
     return render(request, 'pybo/list.html')
 
 def answer_create(request, question_id):
+    '''답변 등록'''
     question = get_object_or_404(Question, pk=question_id)
-    question.answer_set.create(content=request.POST.get('content'),create_date=timezone.now())
-    return redirect('pybo:detail',question_id=question.id)
+    if request.method == 'POST':
+        form = AnswerForm(request.POST)
+        logging.info('1. request.method:{}'.format(request.method))
+        if form.is_valid():
+            logging.info('2. form.is_valid():{}'.format(form.is_valid()))
+            answer = form.save(commit=False)
+            answer.create_date = timezone.now()
+            answer.question = question
+            answer.save()
+            return redirect('pybo:detail', question_id = question.id)
+    else:
+        return HttpResponseNotAllowed('Post 만 가능합니다.')
+
+    # form validation
+    context = {'question':question,'form':form}
+    return render(request,'pybo/question_detail.html',context)
 
 def detail(request, question_id):
     '''question 상세'''
-    print('1. question_id:{}'.format(question_id))
+    logging.info('1. question_id:{}'.format(question_id))
     # question = Question.objects.get(id=question_id)
     question = get_object_or_404(Question, pk=question_id)
-    print('2. question:{}'.format(question))
+    logging.info('2. question:{}'.format(question))
     context = {'question':question}
     return render(request, 'pybo/question_detail.html',context)
 
 def index(request):
     '''question 목록'''
     #list order create_date desc
+    logging.info('index 레벨로 출력')
     question_list = Question.objects.order_by('-create_date')  # order_by('-필드') 마이너스 표시 붙이면 DESC, 없으면 ASC
     #question_list = Question.objects.filter(id=99)   # 없을때 잘 되는지 확인
     context = {'question_list':question_list}
-    print('question_list:{}'.format(question_list))
+    logging.info('question_list:{}'.format(question_list))
     return render(request,'pybo/question_list.html',context)
 
