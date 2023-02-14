f73a4859684a9a8903e916309d49f4ef5a0839d4
cklee90 <cklee90@gmail.com>
2023-02-14 11:04:30 +0900

    settings 파일 분리

---
+++ config/settings/local.py
@@ -0,0 +1,3 @@
+from .base import *  # base 에 있는 모든내용 사용하고 ALLOWED_HOSTS 만 따로씀
+
+ALLOWED_HOSTS = []
---
+++ config/settings/prod.py
@@ -0,0 +1,4 @@
+from .base import * # base 에 있는 모든내용 사용하고 ALLOWED_HOSTS 만 따로씀
+
+
+ALLOWED_HOSTS = ["13.124.46.151"] #AWS 고정 IP
